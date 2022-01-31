823ceff8aa3aeb47db14d1451b95c35ca908a9d9
GodKelvin <kelvin.lehrback@gmail.com>
2022-01-31 19:30:02 -0300

    resolvendo bug do mergesort

--- mergesort_externo.c
+++ mergesort_externo.c
@@ -57,16 +57,13 @@ void half_file(char *nome_arquivo, char* half_arquivo_1, char *half_arquivo_2)
     freeBuffer(buffer_original_file);
 }
 
-//Dada a quantidade de linhas, armazena numa matriz
-//Receber tambem a posicao da matriz para dar continuidade a insercao
-unsigned long int file_to_matriz(FILE *arquivo, char **matriz, unsigned long int qtd_linhas, unsigned long int pos_matriz)
+unsigned long int new_file_to_matriz(FILE *arquivo, char **matriz, unsigned long int qtd_linhas)
 {
     unsigned long int tam = 0;
     //Nao posso fechar o arquivo nessa funcao, pois perco o ponteiro de linha
-    while(tam < qtd_linhas && (fscanf(arquivo, "%[^\n] ", matriz[pos_matriz]) != EOF))
+    while(tam < qtd_linhas && (fscanf(arquivo, "%[^\n] ", matriz[tam]) != EOF))
     {
         //Avanco a posicao da matriz e informo que li uma linha
-        pos_matriz++;
         tam++;
     }
     /*Informo quantas linhas eu li,
@@ -95,8 +92,39 @@ void delete_empty_file(char *nome_arquivo)
     }
 }
 
-//Recebe o tamanho das rodadas, arquivos de origem e de destino
-//A quantidade de linhas dobra a cada rodada.
+void new_merging(FILE *arquivo, char **matriz_1, char **matriz_2, unsigned long int tam_1, unsigned long int tam_2)
+{
+    unsigned long int pos_1 = 0;
+    unsigned long int pos_2 = 0;
+
+    while((pos_1 < tam_1) && (pos_2 < tam_2))
+    {
+        if(strcmp(matriz_1[pos_1], matriz_2[pos_2]) < 0)
+        {
+            fprintf(arquivo, "%s\n", matriz_1[pos_1]);
+            pos_1 += 1;
+        }
+        else
+        {
+            fprintf(arquivo, "%s\n", matriz_2[pos_2]);
+            pos_2 += 1;
+        }            
+    }
+
+    while(pos_1 < tam_1)
+    {
+        fprintf(arquivo, "%s\n", matriz_1[pos_1]);   
+        pos_1 += 1;
+    }
+
+    while(pos_2 < tam_2)
+    {
+        fprintf(arquivo, "%s\n", matriz_2[pos_2]);
+        pos_2 += 1;
+    }
+}
+
+//Quantidade de linhas dobra a cada RUN
 void run_merging(unsigned long int qtd_linhas, char *origem_1, char *origem_2, char *dest_1, char *dest_2)
 {
     FILE *arq_origem_1 = fopen(origem_1, "r");
@@ -121,33 +149,12 @@ void run_merging(unsigned long int qtd_linhas, char *origem_1, char *origem_2, c
         printf("ERROR ARQUIVO: %s\n", dest_2); return;
     }
 
-    unsigned long int pos_matriz = 0;
-    /*EX: Rodada 1 == 1 linhas de cada arquivo.
-    1 linhas de cada arquivo == 2 posicoes na matriz.
-    
-    Rodada 2 == 2 linhas de cada arquivo.
-    2 linhas de cada arquivo == 4 posicoes na matriz.
-    
-    Rodada 3 == 4 linhas de cada arquivo.
-    4 linhas de cada arquivo == 8 posicoes na matriz*/
-    //unsigned long int qtd_linhas = number_rodadas * 2;
+    unsigned long int tamanho_matriz = qtd_linhas;
 
-    unsigned long int tamanho_origem_1, tamanho_origem_2, soma_tamanho_origens;
-    tamanho_origem_1 = calcula_tamanho_arquivo(origem_1);
-    tamanho_origem_2 = calcula_tamanho_arquivo(origem_2);
-    soma_tamanho_origens = tamanho_origem_1 + tamanho_origem_2;
-
-    unsigned long int tamanho_matriz = qtd_linhas * 2;
-
-    /*Se chegou numa rodada cujo o dobro da matriz eh totalmente desproporcional,
-    faco com que a matriz seja do tamanho maximo da soma de ambos os arquivos*/
-    if(tamanho_matriz > soma_tamanho_origens)
-    {
-        tamanho_matriz = soma_tamanho_origens;
-    }
-
-    char **matriz = NULL;
-    unsigned long int linhas_lidas = 0;
+    char **matriz_1 = NULL;
+    char **matriz_2 = NULL;
+    unsigned long int linhas_lidas_1 = 0;
+    unsigned long int linhas_lidas_2 = 0;
     //Comeca com 1 apenas para iniciar o loop
     unsigned long int total_linhas_lidas = 1;
 
@@ -159,65 +166,61 @@ void run_merging(unsigned long int qtd_linhas, char *origem_1, char *origem_2, c
     int read_file_1 = 0;
     int read_file_2 = 0;
     printf("[...Nova Rodada...]\n");
-    //printf("%ld\n", qtd_linhas);
     while(total_linhas_lidas)
     {
-        matriz = cria_matriz(tamanho_matriz);
+        matriz_1 = cria_matriz(tamanho_matriz);
+        matriz_2 = cria_matriz(tamanho_matriz);
 
         //Reseto as variaveis utilizadas
         total_linhas_lidas = 0;
-        pos_matriz = 0;
-        linhas_lidas = 0;
+        linhas_lidas_1 = 0;
+        linhas_lidas_2 = 0;
 
         //Leio da origem 1
-        linhas_lidas = file_to_matriz(arq_origem_1, matriz, qtd_linhas, pos_matriz);
-        if(linhas_lidas)
+        linhas_lidas_1 = new_file_to_matriz(arq_origem_1, matriz_1, qtd_linhas);
+        if(linhas_lidas_1)
         { 
             read_file_1 = 1;
         }
-        total_linhas_lidas += linhas_lidas;
-        pos_matriz = linhas_lidas;
+        total_linhas_lidas += linhas_lidas_1;
+        //pos_matriz = linhas_lidas;
 
         //Leio da origem 2
-        linhas_lidas = file_to_matriz(arq_origem_2, matriz, qtd_linhas, pos_matriz);
-        if(linhas_lidas)
+        linhas_lidas_2 = new_file_to_matriz(arq_origem_2, matriz_2, qtd_linhas);
+        if(linhas_lidas_2)
         {   
             read_file_2 = 1;
         }
         //if(linhas_lidas) read_file_2 = 1;
-        total_linhas_lidas += linhas_lidas;
+        total_linhas_lidas += linhas_lidas_2;
         
         //Se eu li somente de um arquivo, ja esta tudo ordenado!
         if(!read_file_1 || !read_file_2)
         {
-            free_matriz(matriz, tamanho_matriz);
+            free_matriz(matriz_1, tamanho_matriz);
+            free_matriz(matriz_2, tamanho_matriz);
             break;
         }
-
-        //Ordeno os elementos
-        quick_sort_string(matriz, 0, total_linhas_lidas-1);
         //Salvo no arquivo dependendo da flag
         if(flag_file)
         {
-            append_matriz_to_file(arq_destino_1, matriz, total_linhas_lidas);
+            new_merging(arq_destino_1, matriz_1, matriz_2, linhas_lidas_1, linhas_lidas_2);
             flag_file = 0;
             write_file_1 = 1;
         }
         else
         {
-            append_matriz_to_file(arq_destino_2, matriz, total_linhas_lidas);
+            new_merging(arq_destino_2, matriz_1, matriz_2, linhas_lidas_1, linhas_lidas_2);
             flag_file = 1;
             write_file_2 = 1;
         }
-        //free_matriz(matriz, total_linhas_lidas);
-        free_matriz(matriz, tamanho_matriz);
+        free_matriz(matriz_1, tamanho_matriz);
+        free_matriz(matriz_2, tamanho_matriz);
     }
 
     //Se eu escrevi nos dois arquivos, entao nao terminou
     if(write_file_1 && write_file_2 && read_file_1 && read_file_2)
-    {
-        
-        
+    {   
         fclose(arq_origem_1);
         fclose(arq_origem_2);
         fclose(arq_destino_1);
@@ -251,9 +254,6 @@ void run_merging(unsigned long int qtd_linhas, char *origem_1, char *origem_2, c
         delete_empty_file(origem_2);
         
     }
-    
-
-
 }
 
 //void mergeSortExterno()
